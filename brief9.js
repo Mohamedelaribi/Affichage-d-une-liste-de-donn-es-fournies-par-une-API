b44066b6041b1c956c351615ca264ab0e0c895c8
Mohamedelaribi <93386582+Mohamedelaribi@users.noreply.github.com>
2022-01-14 15:04:37 +0100

    brief-9

--- brief9.js
+++ brief9.js
@@ -1,17 +1,27 @@
 var select = document.getElementsByName('top');
 var content = document.getElementById('content');
+var dt;
+var artist 
 
-
-var artist = select.values;
-var request = "https://www.theaudiodb.com/api/v1/json/2/search.php?s=" + artist;
-select.addEventListener('change',()=>
+var request = "https://www.theaudiodb.com/api/v1/json/2/search.php?s=" ;
+select[0].addEventListener('change',()=>
 {
-    fetch(request+artist)
+    artist = select[0].value;
+    fetch(request + artist)
     .then(response=>response.json())
-    .then (response=>{
-        content.innerHTML=
+    .then (data => {
+        dt = data;
+        content.innerHTML+=`<div class='col-8 container-fluid'>
+            <h1>${data.artists[0].strArtist}</h1>
+            <img src=${data.artists[0].strArtistThumb}></img>
+            <p>${data.artists[0].strFacebook}</p>
+            <p>${data.artists[0].intFormedYear}</p>
+            <p>${data.artists[0].strBiographyEN}</p>
+        </div>`
+
 
     })
 }
 )
+
 
